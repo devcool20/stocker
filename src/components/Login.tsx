8d641543858607d870f161ff56107019a337ac04
Divyanshu Sharma <sharmadivyanshu265@gmail.com>
2024-07-15 12:29:53 +0530

    login page improvement

---
+++ src/components/Login.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Alert, ImageBackground } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+type RootStackParamList = {
+  StockList: undefined;
+};
+
+type NavigationProp = NativeStackNavigationProp<RootStackParamList, 'StockList'>;
+
+const Login: React.FC = () => {
+  const navigation = useNavigation<NavigationProp>();
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleLogin = () => {
+    if (email === '' || password === '') {
+      Alert.alert('Error', 'Please enter both email and password.');
+      return;
+    }
+    // Mock login logic
+    if (email === 'admin' && password === 'admin') {
+      //Alert.alert('Success', 'Login Successful!');
+      navigation.navigate('StockList'); // Replace 'StockList' with your desired screen name
+    } else {
+      Alert.alert('Error', 'Invalid email or password.');
+    }
+  };
+
+  return (
+
+      <ImageBackground
+        source={require('../assets/stockbg.jpg')}
+        style={styles.backgroundImage}
+      >
+  
+      <View style={styles.container}>
+        <Text style={styles.title}>Login</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        <Button title="Login" onPress={handleLogin} />
+      </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)', // Semi-transparent background to make text readable
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+    textAlign: 'center',
+    color: '#333945',
+  },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+    borderRadius: 4,
+  },
+});
+
+export default Login;
