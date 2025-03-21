e20a12011a76164e0a77e4f365ed868dad783a5a
Divyanshu Sharma <sharmadivyanshu265@gmail.com>
2025-03-21 21:12:26 +0530

    c2

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="StockList">
+      <Stack.Navigator initialRouteName="Login">
 
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="StockList" component={StockList} options={{ title: 'Stocks' }}/>
